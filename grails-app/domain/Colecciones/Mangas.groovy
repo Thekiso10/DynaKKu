9bf8c650f900e39740598212f13a40129c0cf564
Thekiso10 <diogoassuncao@msn.com>
2019-10-10 19:19:35 +0200

    Crear la columna deseado en la tabla Manga

--- grails-app/domain/Colecciones/Mangas.groovy
+++ grails-app/domain/Colecciones/Mangas.groovy
@@ -9,6 +9,7 @@ class Mangas {
 	boolean completado
 	boolean serieAcabada
 	boolean serieConsecutiva
+	boolean deseado
 	
 	int numTomosActuales
 	int numTomosMaximos
@@ -21,10 +22,34 @@ class Mangas {
 	Demografia demografia
 	
 	static constraints = {
-	
+		nombreManga	nullable: true, blank: true
+		urlImg		nullable: true, blank: true
+
+		completado			nullable: true, blank: true
+		serieAcabada		nullable: true, blank: true
+		serieConsecutiva	nullable: true, blank: true
+		deseado				nullable: true, blank: true
+
+		numTomosActuales	nullable: true, blank: true
+		numTomosMaximos		nullable: true, blank: true
+		precioTotal			nullable: true, blank: true
+
+		fechaInscripcion	nullable: true, blank: true
+		ultimaModificacion	nullable: true, blank: true
+
+		autor		nullable: true, blank: true
+		demografia	nullable: true, blank: true
 	}
 	
 	static mapping = {
 		id generator: 'uuid', Length: 32 //Para generar un id auto-incrementable
+
+		completado			defaultValue: '0'
+		serieAcabada		defaultValue: '0'
+		serieConsecutiva	defaultValue: '0'
+		deseado				defaultValue: '0'
+
+		fechaInscripcion	defaultValue: new Date()
+		ultimaModificacion	defaultValue: new Date()
 	}
 }
