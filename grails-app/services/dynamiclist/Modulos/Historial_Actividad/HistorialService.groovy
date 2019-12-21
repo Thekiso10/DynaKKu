6cc4c6486e10ec73d129f5006c6a6ee0c86c7783
Thekiso10 <diogoassuncao@msn.com>
2019-12-21 22:11:35 +0100

    Hemos creado el nuevos archivo HistorialService

---
+++ grails-app/services/dynamiclist/Modulos/Historial_Actividad/HistorialService.groovy
@@ -0,0 +1,17 @@
+package dynamiclist.Modulos.Historial_Actividad
+
+import Colecciones.Autor
+import Colecciones.Mangas
+import Modulos.Historial_Actividad.HistorialModulos
+import Modulos.Historial_Actividad.HistorialAutor
+import Modulos.Historial_Actividad.HistorialMangas
+import Modulos.Historial_Actividad.HistorialMangasActividad
+import grails.transaction.Transactional
+
+@Transactional
+class HistorialService {
+
+    def serviceMethod() {
+
+    }
+}
