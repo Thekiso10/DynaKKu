472b25cb0344f6ec7c79d8d51482e9e41807e1cf
Thekiso10 <diogoassuncao@msn.com>
2020-02-23 16:12:22 +0100

    Hacer que el al crear y editat un Manga salga un Spinner

--- web-app/js/spinner.js
+++ web-app/js/spinner.js
@@ -8,8 +8,16 @@ function stopSpinner(){
 
 $('#formCreateAutor').submit(function (event) {
     callSpinner();
-})
+});
 
 $('#formEditAutor').submit(function (event) {
     callSpinner();
-})
+});
+
+$('#formCreateMagas').submit(function (event) {
+    callSpinner();
+});
+
+$('#formEditMangas').submit(function (event) {
+    callSpinner();
+});
