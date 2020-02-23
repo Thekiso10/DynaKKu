bdfca56ed579fd01fe4da28e6358f9eaabb70485
Thekiso10 <diogoassuncao@msn.com>
2020-02-23 16:28:07 +0100

    Eventos de los iText colocar variable del texto

--- grails-app/services/dynamiclist/Funciones/ClasesJavaPDF/HeaderFooterPageEvent.java
+++ grails-app/services/dynamiclist/Funciones/ClasesJavaPDF/HeaderFooterPageEvent.java
@@ -14,6 +14,7 @@ import java.util.Date;
 class HeaderFooterPageEvent extends PdfPageEventHelper{
     private PdfTemplate t;
     private Image total;
+    private String text;
 
     public void onOpenDocument(PdfWriter writer, Document document) {
         t = writer.getDirectContent().createTemplate(30, 16);
