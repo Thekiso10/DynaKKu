fa68d58e9682701098940fe202403e706820224e
Thekiso10 <diogoassuncao@msn.com>
2020-02-25 20:13:07 +0100

    Hacer la creacion de la tabla de los roles y la configuracion basica del nuevo plugin

--- grails-app/domain/Modulos/Personalizacion_Usuario/Usuario.groovy
+++ grails-app/domain/Modulos/Personalizacion_Usuario/Usuario.groovy
@@ -1,15 +1,32 @@
 package Modulos.Personalizacion_Usuario
 
-class Usuario {
+import Security.Role
+import Security.UserRole
 
+class Usuario implements Serializable {
+
+    private static final long serialVersionUID = 1
+
+    transient springSecurityService
+
+    /* Valores basicos */
     String id
     String nombre
     String apellido
+    String mail
     String idiomaDefault
-
+    /* Configuración del tema */
     boolean modoDark
-
+    /* Configuracion fechas */
     Date ultimaModificacion
+    Date fechaCreacion
+    /* Seguredad */
+    String username
+    String password
+    boolean enabled
+    boolean accountExpired
+    boolean accountLocked
+    boolean passwordExpired
 
 
     static constraints = {
@@ -17,11 +34,59 @@ class Usuario {
         apellido        nullable: true, blank: true
         idiomaDefault   nullable: true, blank: true
         modoDark        nullable: true, blank: true
+        mail            nullable: true, blank: true
+
+        username 	    blank: false, unique: true
+        password 	    blank: false, nullable: false
 
         ultimaModificacion	nullable: true, blank: true
     }
 
     static mapping = {
-        id generator: 'uuid', Length: 32 //Para generar un id auto-incrementable
+        id          generator: 'uuid', Length: 32 //Para generar un id auto-incrementable
+        password 	column: '`password`'
+        enabled     defaultValue: '1'
     }
+
+    Usuario(String username, String password) {
+        this()
+        this.username = username
+        this.password = password
+    }
+
+    @Override
+    int hashCode() {
+        username?.hashCode() ?: 0
+    }
+
+    @Override
+    boolean equals(other) {
+        is(other) || (other instanceof Usuario && other.username == username)
+    }
+
+    @Override
+    String toString() {
+        username
+    }
+
+    Set<Role> getAuthorities() {
+        UserRole.findAllByUser(this)*.role
+    }
+
+    def beforeInsert() {
+        encodePassword()
+    }
+
+    def beforeUpdate() {
+        if (isDirty('password')) {
+            encodePassword()
+        }
+    }
+
+    protected void encodePassword() {
+        password = springSecurityService?.passwordEncoder ? springSecurityService.encodePassword(password) : password
+    }
+
+    static transients = ['springSecurityService']
+
 }
