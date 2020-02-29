5db956961d6f4514314ea84d12d1696b545eccd0
Thekiso10 <diogoassuncao@msn.com>
2020-02-29 17:05:39 +0100

    bug Sourcetree

--- grails-app/controllers/API/ApiController.groovy
+++ grails-app/controllers/API/ApiController.groovy
@@ -1,6 +1,13 @@
 package API
 
+import grails.converters.JSON
+import grails.plugin.springsecurity.annotation.Secured
+
 class ApiController {
 
-    def index() { }
+    @Secured (['ROLE_ADMIN', 'ROLE_WS'])
+    def getVersion() {
+        def version = ['versión': grailsApplication.config.dynaKKu.version.app]
+        render (status: 200, text: version as JSON, contentType: "application/json")
+    }
 }
